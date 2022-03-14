08cab652c9687a3fafc90eaa2dac28b7d99541bb
ramriddlez <ramankangz@outlook.com>
2022-03-14 17:11:38 -0400

    added blog post model

---
+++ models/BlogPost.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create BlogPost model as Object
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: "blog_post",
+    freezeTableName: true,
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = BlogPost;
