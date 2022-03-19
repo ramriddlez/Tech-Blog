47e9ba815efdd6ba7ad6bda5990d4c1a1eb8025f
ramriddlez <ramankangz@outlook.com>
2022-03-19 19:35:45 -0400

    altered routes and javascript functions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,85 +26,85 @@ router.get('/', async (req,res) => {
     }
 });
 
-// router.get('/post/:id', async (req,res) => {
-//     try {
-//         const postData = await BlogPost.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['username']
-//                 },
-//             ],
-//         });
+router.get('/posts/:id', async (req,res) => {
+    try {
+        const postData = await BlogPost.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ],
+        });
 
-//         const post = postData.get({ plain: true})
+        const post = postData.get({ plain: true})
 
-//         if(postData.comment_count != 0) {
-//             const postData = await Post.findByPk(req.params.id, {
-//                 include: [
-//                     {
-//                         model: User,
-//                         attributes: ['username'],
-//                     },
-//                     {
-//                         model: Comment,
-//                         attributes: ['content','date_created','user_id'],
-//                     },
-//                 ],
-//             });
+        if(postData.comment_count != 0) {
+            const postData = await BlogPost.findByPk(req.params.id, {
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                    {
+                        model: Comment,
+                        attributes: ['content','date_created','user_id'],
+                    },
+                ],
+            });
 
-//             const post = postData.get({ plain: true });
+            const post = postData.get({ plain: true });
 
-//             console.log(post);
+            console.log(post);
 
-//             const user_ids = [];
-//             const user_names = [];
-//             const comments = [];
+            const user_ids = [];
+            const user_names = [];
+            const comments = [];
 
-//             for(i=0;i<post.comments.length;i++){
-//                 let user_id = post.comments[i].user_id;
-//                 user_ids.push(user_id);
-//             }
+            for(i=0;i<post.comments.length;i++){
+                let user_id = post.comments[i].user_id;
+                user_ids.push(user_id);
+            }
 
-//             console.log(user_ids);
+            console.log(user_ids);
 
-//             for(i=0;i<user_ids.length;i++){
-//                 const usercommentData = await User.findByPk(user_ids[i], { attributes:['username'] });
+            for(i=0;i<user_ids.length;i++){
+                const usercommentData = await User.findByPk(user_ids[i], { attributes:['username'] });
 
-//                 const user_name = usercommentData.get({ plain: true });
+                const user_name = usercommentData.get({ plain: true });
 
-//                 user_names.push(user_name);
-//             }
+                user_names.push(user_name);
+            }
 
-//             console.log(user_names);
+            console.log(user_names);
 
 
-//             for(i=0;i<postData.comments.length;i++) {
-//                 const comment = {
-//                     content: postData.comments[i].content,
-//                     date_created: postData.comments[i].date_created,
-//                     user_id: postData.comments[i].user_id,
-//                     user_name: user_names[i].username,
-//                 }
+            for(i=0;i<postData.comments.length;i++) {
+                const comment = {
+                    content: postData.comments[i].content,
+                    date_created: postData.comments[i].date_created,
+                    user_id: postData.comments[i].user_id,
+                    user_name: user_names[i].username,
+                }
 
-//                 comments.push(comment);
-//             }
+                comments.push(comment);
+            }
             
-//             console.log(comments);
+            console.log(comments);
 
-//             res.render('post', {
-//                 ...post, comments, loggedIn: req.session.loggedIn
-//             });
-//         } else {
-//             console.log(post);
-//             res.render('post', {
-//                 ...post, loggedIn: req.session.loggedIn
-//             });
-//         }
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+            res.render('postInfo', {
+                ...post, comments, loggedIn: req.session.loggedIn
+            });
+        } else {
+            console.log(post);
+            res.render('postInfo', {
+                ...post, loggedIn: req.session.loggedIn
+            });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
