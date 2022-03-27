d78216496c113030ff8d9a57c874431baf26787c
ramriddlez <ramankangz@outlook.com>
2022-03-26 23:07:53 -0400

    added routing fix

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -3,7 +3,7 @@ const { BlogPost, User, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 const sequelize = require('../../config/connection');
 
-router.get('/create',  withAuth, async (req,res) => {
+router.get('/create', async (req,res) => {
     try{
 
         res.render('createpost', {loggedIn: req.session.loggedIn});
@@ -55,7 +55,23 @@ router.get('/:id', (req, res) => {
   });
 
 
+  router.post('/create', async (req, res) => {
+    try{
+        const postData = await BlogPost.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+    
+        console.log(postData);
+    
+        res.json({ message: 'Post successfully created!' });
+        res.render('dashboard', {loggedIn: req.session.loggedIn});
 
+    } catch (err) {
+            res.status(500).json(err);
+    }
+});
 
 router.get('/edit/:id', withAuth, async (req,res) => {
     try{
@@ -80,89 +96,6 @@ router.get('/edit/:id', withAuth, async (req,res) => {
     }
 });
 
-router.get('/:id', async (req,res) => {
-    try {
-        const postData = await BlogPost.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User, 
-                    attributes: ['username'],
-                },
-                {
-                    model: BlogPost,
-                    attributes: ['title, content, comment_count, date_created']
-                },
-            ],
-        });
-
-        const post = postData.get({ plain: true})
-
-        if(postData.comment_count != 0) {
-            const postData = await BlogPost.findByPk(req.params.id, {
-                include: [
-                    {
-                        model: User,
-                        attributes: ['username'],
-                    },
-                    {
-                        model: Comment,
-                        attributes: ['content','date_created','user_id'],
-                    },
-                ],
-            });
-
-            const post = postData.get({ plain: true });
-
-            console.log(post);
-
-            const user_ids = [];
-            const user_names = [];
-            const comments = [];
-
-            for(i=0;i<post.comments.length;i++){
-                let user_id = post.comments[i].user_id;
-                user_ids.push(user_id);
-            }
-
-            console.log(user_ids);
-
-            for(i=0;i<user_ids.length;i++){
-                const usercommentData = await User.findByPk(user_ids[i], { attributes:['username'] });
-
-                const user_name = usercommentData.get({ plain: true });
-
-                user_names.push(user_name);
-            }
-
-            console.log(user_names);
-
-
-            for(i=0;i<postData.comments.length;i++) {
-                const comment = {
-                    content: postData.comments[i].content,
-                    date_created: postData.comments[i].date_created,
-                    user_id: postData.comments[i].user_id,
-                    user_name: user_names[i].username,
-                }
-
-                comments.push(comment);
-            }
-            
-            console.log(comments);
-
-            res.render('postInfo', {
-                ...post, comments, loggedIn: req.session.loggedIn
-            });
-        } else {
-            console.log(post);
-            res.render('postInfo', {
-                ...post, loggedIn: req.session.loggedIn
-            });
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
 
 
 router.post('/create', async (req, res) => {
@@ -181,7 +114,7 @@ router.post('/create', async (req, res) => {
             return;
         }
 
-        res.render('createpost', {
+        res.render('dashboard', {
             loggedIn: req.session.loggedIn
         });
 
