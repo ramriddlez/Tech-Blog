bf3f725e7f039437b7973c4806a9ff839c8da54e
ramriddlez <ramankangz@outlook.com>
2022-03-26 23:56:39 -0400

    fixed server issue

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,38 +1,41 @@
 const router = require('express').Router();
-const  { User, BlogPost, Comment } = require('../models');
+const { User, BlogPost, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/connection');
 
 
-router.get('/dashboard', withAuth, async (req,res) => {
-    BlogPost.findAll({
-        where: {
-          // use the ID from the session
-          id: req.session.id
-        },
-        attributes: [
-          'id',
-          'title',
-          'date_created',
-          'content'
-        ],
-        include: [
-          {
-            model: Comment,
-            attributes: ['id', 'content', 'post_id', 'user_id', 'date_created'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          }]
-      })
-        .then(dbPostData => {
-          // serialize data before passing to template
-          const posts = dbPostData.map(post => post.get({ plain: true }));
-          res.render('dashboard', { posts, loggedIn: true });
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+
+router.get('/', withAuth, async (req, res) => {
+  BlogPost.findAll({
+    where: {
+      // use the ID from the session
+      id: req.session.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'date_created',
+      'content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'content', 'post_id', 'user_id', 'date_created'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
+
+module.exports = router;
